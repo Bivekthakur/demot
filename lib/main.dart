2ca6f9d037c6175c5e24f57204fca47234fe4462
Bivek Thakur <19121A05T9@vidyanikethan.asia>
2022-08-14 13:07:40 +0530

    day7

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:google_fonts/google_fonts.dart';
+
 import 'pages/login_page.dart';
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -13,18 +15,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
-        
       ),
-      initialRoute: "/home",
+      //initialRoute: "/home",
       routes: {
-        "/": (context) => LoginPage(), 
-        "/home":(context) => HomePage(),
-        "/login":(context) => LoginPage()
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
       },
-
     );
   }
 }
